1d483ca922f8df27c9fe22ddb6b230e5fca8a85d
Mikhail Arkhipov <zet.ten@mail.ru>
2023-10-02 17:13:28 +0300

    notes

--- app.ts
+++ app.ts
@@ -1,3 +1,7 @@
 const skill: readonly [number, string] = [1, "Dev"]; // пример использования, приходит конфигурация в виде массива, и мы не хотим, чтобы ее использовали
 
 const skills: ReadonlyArray<string> = ["Dev", "Devops"];
+
+
+// skill[0] = 2 - нельзя переприсваивать
+// skills.push('asd') - нельзя пушить
