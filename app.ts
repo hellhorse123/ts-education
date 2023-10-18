3b6bcc9f636af08b83ed25581c58bf4655bb2f45
Mikhail Arkhipov <zet.ten@mail.ru>
2023-10-18 11:07:44 +0300

    conc

--- app.ts
+++ app.ts
@@ -1,1 +1,31 @@
-let w = 1;
+function logId(id: string | number): void {
+  console.log(id);
+}
+
+const a = logId(1);
+
+function multiply(f: number, s?: string) {
+  //если внутри функции мы хоть где-то что-то возвращаем, то она будет alias между тем что мы вернули и undefined, иначе void
+  if (!s) {
+    return f * f;
+  }
+}
+
+type voidFunction = () => void;
+
+const f1: voidFunction = () => {};
+
+const f2: voidFunction = () => {
+  //такая запись с типом функции void означает, что мы можем вернуть все что угодно, но оно будет игнорироваться.
+  return true;
+};
+
+const b = f2(); // b - это void
+
+const skills = ["Dev", "DevOps"];
+
+const user = {
+  s: ["s"],
+};
+
+skills.forEach((skill) => user.s.push(skill));
