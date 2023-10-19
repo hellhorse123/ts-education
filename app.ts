fb5fad4d0ad1e4b5e06f6f783ba7c2492dfd5500
Mikhail Arkhipov <zet.ten@mail.ru>
2023-10-19 09:43:39 +0300

    conc

--- app.ts
+++ app.ts
@@ -1,31 +1,41 @@
-function logId(id: string | number): void {
-  console.log(id);
-}
+let input: unknown;
 
-const a = logId(1);
+input = 3;
+input = ["as", "asd"];
 
-function multiply(f: number, s?: string) {
-  //если внутри функции мы хоть где-то что-то возвращаем, то она будет alias между тем что мы вернули и undefined, иначе void
-  if (!s) {
-    return f * f;
+// при типе unknown мы не сможем присвоить/положить в другую переменную, где задан тип (пока не укажем тип)
+// let res: string = input;
+
+function run(i: unknown) {
+  if (typeof i == "number") {
+    i++;
+  } else {
+    i;
   }
 }
 
-type voidFunction = () => void;
+run(input);
 
-const f1: voidFunction = () => {};
+async function getData() {
+  try {
+    await fetch("");
+  } catch (e) {
+    if (e instanceof Error) {
+      console.log(e.message);
+    }
+  }
+}
+//вторая проверка хуже, так как мы явно приводим ошибку к Error (а такого может не быть)
+async function getDataForce() {
+  try {
+    await fetch("");
+  } catch (error) {
+    const e = error as Error;
+    console.log(e.message);
+  }
+}
 
-const f2: voidFunction = () => {
-  //такая запись с типом функции void означает, что мы можем вернуть все что угодно, но оно будет игнорироваться.
-  return true;
-};
 
-const b = f2(); // b - это void
+type U1 = unknown | null; // все, что юнион тип с unknown всегда становится unknown
 
-const skills = ["Dev", "DevOps"];
-
-const user = {
-  s: ["s"],
-};
-
-skills.forEach((skill) => user.s.push(skill));
+type I1 = unknown & string; // intersection с unknown и любого другого типа дает этот тип
