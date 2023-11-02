8bfed4b3e617b3b2760c8283a49722d72d02800f
Mikhail Arkhipov <zet.ten@mail.ru>
2023-11-02 15:00:25 +0300

    conc

--- app.ts
+++ app.ts
@@ -1,12 +1,3 @@
-let a = 5;
-let b: string = a.toString();
-let e: string = new String(a).valueOf(); //с большой буквы - interface конструктора для строки (он уже является объектом)
-let e1: boolean = new Boolean(a).valueOf();
-
-let c = "asd";
-let d: number = +c;
-let d1: number = parseInt(c);
-
 interface User {
   name: string;
   email: string;
@@ -19,31 +10,48 @@ const user: User = {
   login: "vasasas",
 };
 
-const user2 = {
-  name: "Vasya",
-  email: "vas@mail.ru",
-  login: "vasasas",
-} as User; //равносильно первой записи
-
-const user3 = <User>{
-  name: "Vasya",
-  email: "vas@mail.ru",
-  login: "vasasas",
-}; // могут быть проблемы такой записи в React , так как <> зарезервировано как компонент JSX.Element
-
 interface Admin {
   name: string;
   role: number;
 }
 
-const admin: Admin = {
-  ...user,
-  role: 1,
-}; // при преобразовании в js,  у админа будут лишние свойства (email, login), что может вызвать нежелательные сайд эффекты
+function LogId(id: string | number) {
+  if (typeof id === "string") {
+    console.log(id); //string
+  } else {
+    console.log(id); //number
+  }
+}
 
-function userToAdmin(user: User): Admin {
-  return {
-    name: user.login,
-    role: 1,
-  };
-} // явное преобразование, которое сохраняет только необходимые поля
+function isString(x: string | number): x is string {
+  return typeof x === "string";
+}
+
+function LogIdGuard(id: string | number) {
+  if (isString(id)) {
+    console.log(id);
+  } else {
+    console.log(id);
+  }
+}
+
+//___
+
+function isAdmin(user: User | Admin): user is Admin {
+  return "role" in user;
+}
+
+function isAdminAlternative(user: User | Admin): user is Admin {
+  return (user as Admin).role !== undefined;
+}
+
+function setRoleZero(user: User | Admin) {
+  if (isAdmin(user)) {
+    user.role = 0;
+  } else {
+    throw new Error("user is not admin");
+  }
+}
+
+
+// Type Guard позволяет оградить поток от ненужных типов
