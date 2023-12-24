4bd416d9ce75eb1083c566b69aad09211132da3f
Mikhail Arkhipov <zet.ten@mail.ru>
2023-12-24 13:54:52 +0300

    conc

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 "use strict";
+//отличие конструктора от обычных методов:
+// - конструктор автоматически триггерится, когда мы используем new (тем самым вызываем конструктор и передаем туда параметр, указанный в конструкторе), после этого параметр присваивается инстансу нашего user
+// - в результате конструктор должен всегда возвращать instance этого класса (в данном случае User). (нельзя переопределить возвращаемое значение)
+// - конструктор не может принимать generic параметр (его может принимать класс)
+// - конструктор можно сделать overload (имея описанную сигнатуру функции дополняем ее альтернативной сигнатурой)
 class User {
-    constructor(name) {
-        this.name = name;
+    constructor(ageOrName, age) {
+        if (typeof ageOrName === "string") {
+            this.name = ageOrName;
+        }
+        else if (typeof ageOrName === "number") {
+            this.age = ageOrName;
+        }
+        if (typeof age === "number") {
+            this.age = age;
+        }
     }
 }
 const user = new User("Shan");
-console.log(user);
-user.name = "Ben";
-console.log(user);
+const user2 = new User();
+const user3 = new User(33);
+const user4 = new User("Ben", 34);
--- app.ts
+++ app.ts
@@ -1,22 +1,31 @@
-class User {
-  name: string; //оп умолчанию в ts необходимо инициализировать свойства класса
+//отличие конструктора от обычных методов:
+// - конструктор автоматически триггерится, когда мы используем new (тем самым вызываем конструктор и передаем туда параметр, указанный в конструкторе), после этого параметр присваивается инстансу нашего user
+// - в результате конструктор должен всегда возвращать instance этого класса (в данном случае User). (нельзя переопределить возвращаемое значение)
+// - конструктор не может принимать generic параметр (его может принимать класс)
 
-  constructor(name: string) {
-    this.name = name;
+// - конструктор можно сделать overload (имея описанную сигнатуру функции дополняем ее альтернативной сигнатурой)
+
+class User {
+  name: string;
+  age: number;
+
+  constructor();
+  constructor(name: string);
+  constructor(age: number);
+  constructor(name: string, age: number);
+  constructor(ageOrName?: string | number, age?: number) {
+    if (typeof ageOrName === "string") {
+      this.name = ageOrName;
+    } else if (typeof ageOrName === "number") {
+      this.age = ageOrName;
+    }
+    if (typeof age === "number") {
+      this.age = age;
+    } 
   }
 }
 
 const user = new User("Shan");
-console.log(user);
-user.name = "Ben";
-console.log(user);
-
-
-//1) "strictPropertyInitialization": false in tsconfig
-//2) ! for class prop
-class Admin {
-    role: number;
-}
-const admin = new Admin();
-
-admin.role = 1
+const user2 = new User();
+const user3 = new User(33);
+const user4 = new User("Ben", 34);
