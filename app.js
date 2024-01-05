b230bd498430f60951a341b6e83415f4e5792e94
Mikhail Arkhipov <zet.ten@mail.ru>
2024-01-05 17:23:02 +0300

    conc in lesson 44
    features in extends

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 "use strict";
-class User {
-    // getLogin(l: string) {
-    //   this.login = `user-${l}`;
-    // }
-    //реализация сеттера
-    set login(l) {
-        this._login = `user-${l}`;
+class Payment {
+    constructor(id) {
+        this.status = "new";
+        this.id = id;
     }
-    //реализация геттера
-    get login() {
-        return 'no_login';
+    pay() {
+        this.status = "paid";
     }
 }
-const user = new User();
-// user.login = `user-...` Так мы бы присваивали без геттера, но ООП предполагает, что логин должен бть инкапсулирован внутрь юзера
-user.login = "myLogin";
-console.log(user);
-console.log(user.login);
+class PersistedPayment extends Payment {
+    constructor() {
+        const id = Math.random();
+        super(id); // означает, что мы обращаемся к конструктору класса Payment и начинаем его конструировать с указанным выше идентификатором.
+    }
+    save() {
+        //save in DB
+    }
+    pay(date) {
+        // pay(date?: Date) {
+        super.pay();
+        // this.status = "paid"; вместо явного дублирования используем super как ссылку
+        if (date) {
+            this.paidAt = date;
+        }
+    }
+}
+new PersistedPayment();
+class User {
+    constructor() {
+        this.name = "user";
+        console.log(this.name);
+    }
+}
+class Admin extends User {
+    constructor() {
+        super();
+        this.name = "admin";
+        console.log(this.name);
+    }
+}
+new Admin(); //name = user
--- app.ts
+++ app.ts
@@ -26,8 +26,8 @@ class PersistedPayment extends Payment {
     //save in DB
   }
 
-override  pay(date?: Date) {
-  // pay(date?: Date) {
+  override pay(date?: Date) {
+    // pay(date?: Date) {
     super.pay();
     // this.status = "paid"; вместо явного дублирования используем super как ссылку
     if (date) {
@@ -37,3 +37,32 @@ override  pay(date?: Date) {
 }
 
 new PersistedPayment();
+
+class User {
+  name: string = "user";
+
+  constructor() {
+    console.log(this.name);
+  }
+}
+
+class Admin extends User {
+  name: string = "admin";
+
+  constructor() {
+    super(); // super должен вызываться вначале конструктора из-за того, что мы обращаемся к свойству класса
+    console.log(this.name); //name = admin
+  }
+}
+
+new Admin(); //name = user
+
+new Error("");
+
+class HttpError extends Error {
+  code: number;
+  constructor(message: string, code?: number) {
+    super(message);
+    this.code = code ?? 500;
+  }
+}
