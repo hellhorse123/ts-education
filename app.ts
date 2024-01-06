652d1aacdd811e0dee296e31da57e613187c9dea
Mikhail Arkhipov <zet.ten@mail.ru>
2024-01-06 09:29:10 +0300

    concs

--- app.ts
+++ app.ts
@@ -1,68 +1,59 @@
-type PaymentStatus = "new" | "paid";
+class User {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+//inheritance
+class Users extends Array<User> {
+  searchByName(name: string) {
+    return this.filter((u) => u.name === name);
+  }
+
+  override toString(): string {
+    return this.map((u) => u.name).join(", ");
+  }
+}
+
+const users = new Users();
+users.push(new User("Ben"));
+
+//composition
+class Users2 {
+  users: User[];
+
+  push(u: User) {
+    this.users.push(u);
+  }
+}
+
+//
 
 class Payment {
-  id: number;
-  status: PaymentStatus = "new";
+  date: Date;
+}
 
-  constructor(id: number) {
-    this.id = id;
-  }
+//inheritance
+class UserWithPayment extends Payment {
+  name: string;
+}
 
-  pay() {
-    this.status = "paid";
+//composition
+class UserWithPayment2 {
+  user: User;
+  payment: Payment;
+
+  constructor(user: User, payment: Payment) {
+    this.payment = payment;
+    this.user = user;
   }
 }
 
-class PersistedPayment extends Payment {
-  databaseId: number;
-  paidAt: Date;
-
-  constructor() {
-    const id = Math.random();
-    super(id); // означает, что мы обращаемся к конструктору класса Payment и начинаем его конструировать с указанным выше идентификатором.
-  }
-
-  save() {
-    //save in DB
-  }
-
-  override pay(date?: Date) {
-    // pay(date?: Date) {
-    super.pay();
-    // this.status = "paid"; вместо явного дублирования используем super как ссылку
-    if (date) {
-      this.paidAt = date;
-    }
-  }
-}
-
-new PersistedPayment();
-
-class User {
-  name: string = "user";
-
-  constructor() {
-    console.log(this.name);
-  }
-}
-
-class Admin extends User {
-  name: string = "admin";
-
-  constructor() {
-    super(); // super должен вызываться вначале конструктора из-за того, что мы обращаемся к свойству класса
-    console.log(this.name); //name = admin
-  }
-}
-
-new Admin(); //name = user
-
-new Error("");
-
-class HttpError extends Error {
-  code: number;
-  constructor(message: string, code?: number) {
-    super(message);
-    this.code = code ?? 500;
-  }
-}
+//выводы:
+//inheritance (наследование), когда использовать?
+//1) Когда наследуемся в рамках одной доменной области. Например юзер с кредами и просто пользователь (доменная область - пользователь)
+// Когда НЕ нужно использовать??
+//Когда наследуемся от агрегационных сложных внутренних массивов (Array<User>), однако наследование от утилитарных классов (Error) допустимо.
+//В остальных случаях лучше использовать композицию
