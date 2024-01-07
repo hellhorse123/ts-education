9ed2e086b90fe38cc4bf0a7dec4c5cc3ab93ebe6
Mikhail Arkhipov <zet.ten@mail.ru>
2024-01-07 09:33:57 +0300

    conc

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 "use strict";
+// В JS this ссылается на контекст текущего объекта
+class Payment {
+    constructor() {
+        this.date = new Date();
+        //второй способ не терять контекст - использовать стрелочные функции:
+        this.getDateArrow = () => {
+            return this.date;
+        };
+    }
+    getDate() {
+        return this.date;
+    }
+    getDate1() {
+        // спец параметр this, который явно указывает, к чему принадлежит контекст (не существует после компиляции, является "подсказкой для ts")
+        return this.date;
+    }
+}
+const p = new Payment();
+console.log(p.getDate());
+const user = {
+    id: 1,
+    //   paymentDate: p.getDate, // undefined - был потерян контекст. Cослались на getDate, который возвращает this.date . Соответственно this в контексте объекта user, это и есть этот user (поэтому мы никаким образом не можем обратиться к тому, что это был Payment)
+    paymentDate: p.getDate.bind(p),
+    paymentDateArrow: p.getDateArrow,
+};
+console.log(user.paymentDate());
+console.log(user.paymentDateArrow());
+class PaymentPersistent extends Payment {
+    save() {
+        return super.getDate();
+    }
+}
+console.log(new PaymentPersistent().save());
--- app.ts
+++ app.ts
@@ -1,32 +1,42 @@
-class UserService {
-  static db: any;
-  private static db2: any; // статичное свойство может быть приватным
-//   static name: string = "12"; // ограничение статичного класса: не можем назвать переменную name
+// В JS this ссылается на контекст текущего объекта
 
-  static getUser(id: number) {
-    return UserService.db.findById(id);
+class Payment {
+  private date: Date = new Date();
+
+  getDate() {
+    return this.date;
   }
 
-  static async getUser2(id: number) {
-    // можно использовать асинхронный код вместе со статиком
-    return UserService.db.findById(id);
+  getDate1(this: Payment) {
+    // спец параметр this, который явно указывает, к чему принадлежит контекст (не существует после компиляции, является "подсказкой для ts")
+    return this.date;
   }
 
-  constructor(id: number) {}
+  //второй способ не терять контекст - использовать стрелочные функции:
+  getDateArrow = () => {
+    return this.date;
+  };
+}
 
-  create() {
-    // this.create через ключ слово this доступен только метод create. Тогда как через обращение  UserService доступны все статические свойства
-    UserService.db;
-  }
+const p = new Payment();
+console.log(p.getDate());
 
-  static {
-    // await new Promise не можем использовать асинхронный код внутри статичного блока класса
-    UserService.db = "png";
+const user = {
+  id: 1,
+  //   paymentDate: p.getDate, // undefined - был потерян контекст. Cослались на getDate, который возвращает this.date . Соответственно this в контексте объекта user, это и есть этот user (поэтому мы никаким образом не можем обратиться к тому, что это был Payment)
+  paymentDate: p.getDate.bind(p), // рабочий вариант с bind
+  paymentDateArrow: p.getDateArrow,
+};
+
+console.log(user.paymentDate());
+console.log(user.paymentDateArrow());
+
+class PaymentPersistent extends Payment {
+  save() {
+    // return super.getDate(); - работает корректно
+    // return super.getDateArrow(); - не работает, getDateArrow - не функция. Когда пытаемся обратиться к getDateArrow, мы используем наследование. Так как в прототипе наследования у нас нет этой функции, то мы никак не можем получить ее из исходного объекта. В исходном прототипе из-за наследования мы можем получить обычную функцию, но не можем получить стрелочную функцию
+    return this.getDateArrow(); // - снова работает корректно.
   }
 }
 
-UserService.db; //удобно при паттерне singleton.
-UserService.getUser(1);
-
-const inst = new UserService(1);
-inst.create();
+console.log(new PaymentPersistent().save());
