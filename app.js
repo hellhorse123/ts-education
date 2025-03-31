3786a20538f5c1d06c7c01261b86967ba1fd83ba
Mikhail Arkhipov <48678379+hellhorse123@users.noreply.github.com>
2025-03-31 10:22:01 +0300

    les71

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
 "use strict";
-let w = 1;
+/*
+Функция принимает массив объектов и его ключ, производит группировку по указанному ключу и возвращает сгруппированный объект
+Пример:
+[
+    {group: 1, name: 'a'},
+    {group: 1, name: 'b'},
+    {group: 2, name: 'c'},
+]
+
+После группировки:
+{
+    '1': [{group: 1, name: 'a'}, {group: 1, name: 'b'}]
+    '2': [{group: 2, name: 'c'}]
+}
+*/
+const data = [
+    { group: 1, name: "a" },
+    { group: 1, name: "b" },
+    { group: 2, name: "c" },
+];
+function groupData(array, key) {
+    return array.reduce((map, item) => {
+        const itemKey = item[key];
+        let curEl = map[itemKey];
+        if (Array.isArray(curEl)) {
+            curEl.push(item);
+        }
+        else {
+            curEl = [item];
+        }
+        map[itemKey] = curEl;
+        return map;
+    }, {});
+}
+const res = groupData(data, 'group');
+console.log(res);
--- app.ts
+++ app.ts
@@ -1,1 +1,54 @@
-let w = 1;
+/*
+Функция принимает массив объектов и его ключ, производит группировку по указанному ключу и возвращает сгруппированный объект
+Пример:
+[
+    {group: 1, name: 'a'},
+    {group: 1, name: 'b'},
+    {group: 2, name: 'c'},
+]
+
+После группировки:
+{
+    '1': [{group: 1, name: 'a'}, {group: 1, name: 'b'}]
+    '2': [{group: 2, name: 'c'}]
+}
+*/
+
+interface Data {
+  group: number;
+  name: string;
+}
+
+const data: Data[] = [
+  { group: 1, name: "a" },
+  { group: 1, name: "b" },
+  { group: 2, name: "c" },
+];
+
+interface IGroup<T> {
+  [key: string]: T[];
+}
+
+type key = string | number | symbol;
+
+function groupData<T extends Record<key, any>>(
+  array: T[],
+  key: keyof T
+): IGroup<T> {
+  return array.reduce<IGroup<T>>((map: IGroup<T>, item) => {
+    const itemKey = item[key];
+    let curEl = map[itemKey];
+    if (Array.isArray(curEl)) {
+      curEl.push(item);
+    } else {
+      curEl = [item];
+    }
+
+    map[itemKey] = curEl;
+    return map;
+  }, {});
+}
+
+const res = groupData<Data>(data,'group');
+console.log(res);
+
