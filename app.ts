bc4d5f9843c56fa542e56d70cc8aa377c50a4df5
Mikhail Arkhipov <48678379+hellhorse123@users.noreply.github.com>
2025-03-31 13:20:54 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,54 +1,13 @@
-/*
-Функция принимает массив объектов и его ключ, производит группировку по указанному ключу и возвращает сгруппированный объект
-Пример:
-[
-    {group: 1, name: 'a'},
-    {group: 1, name: 'b'},
-    {group: 2, name: 'c'},
-]
-
-После группировки:
-{
-    '1': [{group: 1, name: 'a'}, {group: 1, name: 'b'}]
-    '2': [{group: 2, name: 'c'}]
-}
-*/
-
-interface Data {
-  group: number;
-  name: string;
+function runTransaction(transaction: { fromTo: [string, string] }) {
+  console.log(transaction);
 }
 
-const data: Data[] = [
-  { group: 1, name: "a" },
-  { group: 1, name: "b" },
-  { group: 2, name: "c" },
-];
+const transaction = {
+  fromTo: ["1", "2"],
+};
 
-interface IGroup<T> {
-  [key: string]: T[];
-}
+runTransaction(transaction);
 
-type key = string | number | symbol;
-
-function groupData<T extends Record<key, any>>(
-  array: T[],
-  key: keyof T
-): IGroup<T> {
-  return array.reduce<IGroup<T>>((map: IGroup<T>, item) => {
-    const itemKey = item[key];
-    let curEl = map[itemKey];
-    if (Array.isArray(curEl)) {
-      curEl.push(item);
-    } else {
-      curEl = [item];
-    }
-
-    map[itemKey] = curEl;
-    return map;
-  }, {});
-}
-
-const res = groupData<Data>(data,'group');
-console.log(res);
-
+type GetFirstArg<T> = T extends (first: infer First, ...args: any[]) => any
+  ? First
+  : never;
