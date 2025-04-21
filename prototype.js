5573f13dafe7ed4f8cdf8426fbfc6e0ec0a6a2f9
Mikhail Arkhipov <zet.ten@mail.ru>
2025-04-21 09:50:51 +0300

    generative patterns

---
+++ prototype.js
@@ -0,0 +1,18 @@
+"use strict";
+class UserHistory {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.createdAt = new Date();
+    }
+    clone() {
+        let target = new UserHistory(this.email, this.name);
+        target.createdAt = this.createdAt;
+        return target;
+    }
+}
+let user = new UserHistory("a@a.ru", "Test1");
+console.log(user);
+let user2 = user.clone();
+user2.email = 'b@b.ru';
+console.log(user2);
---
+++ prototype.ts
@@ -0,0 +1,22 @@
+interface Prototype<T> {
+  clone(): T;
+}
+
+class UserHistory implements Prototype<UserHistory> {
+  createdAt: Date;
+  constructor(public email: string, public name: string) {
+    this.createdAt = new Date();
+  }
+
+  clone(): UserHistory {
+    let target = new UserHistory(this.email, this.name);
+    target.createdAt = this.createdAt;
+    return target;
+  }
+}
+
+let user = new UserHistory("a@a.ru", "Test1");
+console.log(user);
+let user2 = user.clone();
+user2.email = 'b@b.ru';
+console.log(user2);
